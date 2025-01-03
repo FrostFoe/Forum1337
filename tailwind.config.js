5b16ee28c8480067c5f459dc50b2930391d5cb3f
zhour1060 <zhour1060@gmail.com>
2025-01-03 11:55:15 +0000

    Day - 01

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,34 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        customGray: {
-          light: '#2e3039',
-          base: '#eeeeee',
-          dark: '#1f2028',
-        },
+        gray: colors.neutral
       },
-    },
+      fontFamily: {
+        // to change, update font in _document.js
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+        serif: ["var(--font-lora)", ...defaultTheme.fontFamily.serif],
+        stock: [defaultTheme.fontFamily.sans]
+      },
+      aspectRatio: {
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
+        "2/3": "2 / 3",
+        "9/16": "9 / 16"
+      }
+    }
   },
-  darkMode: 'class',
+  variants: {
+    extend: {}
+  },
+  plugins: [require("@tailwindcss/typography")]
 };
